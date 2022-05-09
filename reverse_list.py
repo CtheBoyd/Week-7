37e7d8acf024cd5814fb06df501e293aa8ba9c9b
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-05-09 13:23:17 -0700

    update

---
+++ reverse_list.py
@@ -0,0 +1,17 @@
+# create a list of prime numbers
+reverse_list = [7, -3, 12, 9]
+
+# reverse the order of list elements
+reverse_list.reverse()
+
+
+print('Reversed List:', reverse_list)
+
+# Output: Reversed List: [7, 5, 3, 2]
+
+
+
+
+#vals = [7, -3, 12, 9]
+#reverse_list(vals)
+#print(vals)  # This should print [9, 12, -3, 7]
---
+++ square_list.py
@@ -0,0 +1,13 @@
+def square_val(val):
+  val[] = val[] * val[]
+  print(val)
+
+num_list = [8, 2, 5 , 7]
+square_val(num_list)
+print(num_list)
+
+
+
+nums = [7, -3, 12, 9]
+square_list(nums)
+print(nums)  # This should print [49, 9, 144, 81]
