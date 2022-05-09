9f2584acfd5430a4f22f8241091508782323a179
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-05-09 13:34:47 -0700

    update

--- square_list.py
+++ square_list.py
@@ -1,13 +1,8 @@
-def square_val(val):
-  val[] = val[] * val[]
-  print(val)
-
-num_list = [8, 2, 5 , 7]
-square_val(num_list)
-print(num_list)
+def square_list(nums):
+  return [i ** 2 for i in nums]
 
 
 
 nums = [7, -3, 12, 9]
 square_list(nums)
-print(nums)  # This should print [49, 9, 144, 81]
+print(square_list(nums))  # This should print [49, 9, 144, 81]
